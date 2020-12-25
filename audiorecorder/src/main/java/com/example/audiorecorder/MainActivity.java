ed500d3acc62975d65d83fa03a0399519e0a4559
彭杨 <1033961425@qq.com>
2020-12-25 09:34:44 +0800

    录音文件位置

--- audiorecorder/src/main/java/com/example/audiorecorder/MainActivity.java
+++ audiorecorder/src/main/java/com/example/audiorecorder/MainActivity.java
@@ -9,19 +9,26 @@ import com.example.audiorecorder.base.MyApp;
 import com.zlw.main.recorderlib.RecordManager;
 import com.zlw.main.recorderlib.recorder.RecordConfig;
 import com.zlw.main.recorderlib.recorder.RecordHelper;
+import com.zlw.main.recorderlib.recorder.listener.RecordResultListener;
 import com.zlw.main.recorderlib.recorder.listener.RecordStateListener;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private Button btnStart;
     private Button btnEnd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecordManager.getInstance().init(MyApp.getInstance(), false);
         RecordManager.getInstance().changeFormat(RecordConfig.RecordFormat.MP3);
+        String recordDir = getExternalFilesDir("ppp").getAbsolutePath();
+        Log.i(TAG, "onCreate: recordDir->" + recordDir);
+        RecordManager.getInstance().changeRecordDir(recordDir);
         RecordManager.getInstance().setRecordStateListener(new RecordStateListener() {
             @Override
             public void onStateChange(RecordHelper.RecordState state) {
@@ -31,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onError(String error) {
                 Log.e(TAG, "onError: " + error);
+            }
+        });
+        RecordManager.getInstance().setRecordResultListener(new RecordResultListener() {
+            @Override
+            public void onResult(File result) {
             }
         });
         btnStart = findViewById(R.id.btn_start);
